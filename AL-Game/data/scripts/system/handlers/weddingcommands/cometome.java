629fe1ecfeb592f4e6a1ff045bbe5dd0fca706e2
idhacker542 <noval@hackermail.com>
2016-06-16 19:46:06 +0700

    Fix weddingcommands/cometome.java Fix weddingcommands/missyou.java

--- AL-Game/data/scripts/system/handlers/weddingcommands/cometome.java
+++ AL-Game/data/scripts/system/handlers/weddingcommands/cometome.java
@@ -26,6 +26,7 @@ import com.aionemu.gameserver.utils.chathandlers.WeddingCommand;
 /**
  * @author synchro2
  * @rework Eloann
+ * @rework idhacker542
  */
 public class cometome extends WeddingCommand {
 
@@ -35,43 +36,46 @@ public class cometome extends WeddingCommand {
 
     @Override
     public void execute(final Player player, String... params) {
-
-        Player partner = player.findPartner();
-
-        if (partner == null) {
-            PacketSendUtility.sendMessage(player, "Not online.");
-            return;
-        }
-
-        if (player.isInPrison() || player.isInPvPArena() || partner.isInPrison() || partner.isInPvPArena()) {
-            PacketSendUtility.sendMessage(player, "You cannot use this command in your location.");
-            return;
-        }
-
-        if (player.isInInstance() || partner.isInInstance()) {
-            PacketSendUtility.sendMessage(player, "You can't teleported to " + partner.getName() + ", your partner is in Instance.");
-            return;
-        }
-
-        if (player.isAttackMode() || partner.isAttackMode()) {
-            PacketSendUtility.sendMessage(player, "You can't use this command in combat mode!");
-            return;
-        }
-
-        if (!player.isCommandInUse()) {
-            TeleportService2.teleportTo(partner, player.getWorldId(), player.getInstanceId(), player.getX(), player.getY(), player.getZ(), player.getHeading(), TeleportAnimation.BEAM_ANIMATION);
-            PacketSendUtility.sendMessage(player, partner.getName() + " teleported to you.");
-            player.setCommandUsed(true);
-
-            ThreadPoolManager.getInstance().schedule(new Runnable() {
-                @Override
-                public void run() {
-					player.setCommandUsed(false);
-				}
-			}, 60 * 60 * 1000);
-		}
-		else
-			PacketSendUtility.sendMessage(player, "Only 1 TP per hour.");
+    	
+    	if player.isMarried() {
+    		
+    		Player partner = player.findPartner();
+    		
+    		if (partner == null) {
+    			PacketSendUtility.sendMessage(player, "Not online.");
+    			return;
+    		}
+    		
+    		if (player.isInPrison() || player.isInPvPArena() || partner.isInPrison() || partner.isInPvPArena()) {
+    			PacketSendUtility.sendMessage(player, "You cannot use this command in your location.");
+    			return;
+    		}
+    		
+    		if (player.isInInstance() || partner.isInInstance()) {
+    			PacketSendUtility.sendMessage(player, "You can't teleported to " + partner.getName() + ", your partner is in Instance.");
+    			return;
+    		}
+    		
+    		if (player.isAttackMode() || partner.isAttackMode()) {
+    			PacketSendUtility.sendMessage(player, "You can't use this command in combat mode!");
+    			return;
+    		}
+    		
+    		if (!player.isCommandInUse()) {
+    			TeleportService2.teleportTo(partner, player.getWorldId(), player.getInstanceId(), player.getX(), player.getY(), player.getZ(), player.getHeading(), TeleportAnimation.BEAM_ANIMATION);
+    			PacketSendUtility.sendMessage(player, partner.getName() + " teleported to you.");
+    			player.setCommandUsed(true);
+    			
+    			ThreadPoolManager.getInstance().schedule(new Runnable() {
+    				@Override
+    				public void run() {
+    					player.setCommandUsed(false);
+    				}
+    			}, 60 * 60 * 1000);
+    		}
+    		else
+    			PacketSendUtility.sendMessage(player, "Only 1 TP per hour.");
+    	}
 	}
 
     @Override
