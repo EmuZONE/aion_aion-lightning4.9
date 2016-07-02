c6a5890d88d803ca062cf110e8d8df1aed7f4518
noKyrr <neohunter_@hotmail.com>
2016-07-02 01:36:33 -0300

    Update ClassChangeService.java

--- AL-Game/src/com/aionemu/gameserver/services/ClassChangeService.java
+++ AL-Game/src/com/aionemu/gameserver/services/ClassChangeService.java
@@ -27,6 +27,7 @@ import com.aionemu.gameserver.network.aion.serverpackets.SM_QUEST_ACTION;
 import com.aionemu.gameserver.questEngine.model.QuestState;
 import com.aionemu.gameserver.questEngine.model.QuestStatus;
 import com.aionemu.gameserver.utils.PacketSendUtility;
+import com.aionemu.gameserver.services.SkillLearnService;
 
 /**
  * @author ATracer, sweetkr
